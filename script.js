3b677d528b5a141c65ec7d1e561c20af44c0d0ee
David ADE <david.ade.1989@gmail.com>
2024-07-18 15:41:13 +0200

    add left movement

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const btnRight = document.querySelector('#right');
 
 let score = 0
 let grid = [["","","",""],["","","",""],["","","",""],["","","",""]]
-// let grid = [[2,"","",""],[2,"","",""],[4,"","",""],[4,"","",""]]
 
 
 // Function to return a random number between 0 and 3
@@ -214,6 +213,54 @@ const moveRightClearSpaces = () => {
   }
 }
 
+// Function to move the squares left to clear the spaces
+const moveLeftClearSpaces = () => {
+  console.log("Move Left")
+  for (let i = 0 ; i < 4; i++) {
+    // Check if the row is empty
+    let empty = true;
+    for (let j = 0; j < 4; j++) {
+      if (grid[i][j] !== "") {
+        empty = false;
+      }
+    }
+    // Switch to the next column if the current one is empty
+    if (empty) {
+      continue;
+    }
+    
+    // Begin the movement
+    for (let j = 0; j <= 4; j++) {
+      // Check if the cell is empty
+      if (grid[i][j] === "") {
+
+        // Check the cells below
+        for (let k = j + 1; k <= 3; k++) {
+          if (grid[i][k] !== "") {
+            // Swap the cells
+            grid[i][j] = grid[i][k];
+            grid[i][k] = "";
+            // Update the screen
+            cell = document.querySelector(`#cell-${i}-${j}`);
+            cell.innerText = grid[i][j];
+            cell = document.querySelector(`#cell-${i}-${k}`);
+            cell.innerText = grid[i][k];
+            break;
+          } 
+          // The cell is empty
+          else {
+            continue;
+          }
+        }
+      } 
+      // The cell is not empty, so continue
+      else {
+        continue
+      }
+    }
+  }
+}
+
 // Function for DOWN movement
 const moveDown = () => {
   let tempGrid = JSON.parse(JSON.stringify(grid))
@@ -325,6 +372,43 @@ const moveRight = () => {
   }
 }
 
+// Function for LEFT movement
+const moveLeft = () => {
+  let tempGrid = JSON.parse(JSON.stringify(grid))
+
+  // #1 - Move the squares left
+  moveLeftClearSpaces()
+
+  // #2 - Merge the squares
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      if (grid[i][j] === grid[i][j+1] && grid[i][j] !== "") {
+        grid[i][j] = grid[i][j] * 2;
+        grid[i][j+1] = "";
+        // Update the screen
+        cell = document.querySelector(`#cell-${i}-${j}`);
+        cell.innerText = grid[i][j];
+        cell = document.querySelector(`#cell-${i}-${j+1}`);
+        cell.innerText = "";
+        // Update the score
+        updateScore(grid[i][j]);
+      }
+    }
+  }
+
+  // #3 - Move the squares left
+  moveLeftClearSpaces()
+
+  // #4 - Add a new square only if the grid has changed
+  if (compareArrays(grid, tempGrid)) {
+    console.log("Left movement not possible")
+    messageDisplay.innerText = "Left movement not possible"
+  } else {
+    addSquare()
+    messageDisplay.innerText = ""
+  }
+}
+
 
 // ------------------- INITIALIZATION -------------------
 newGame.addEventListener('click', () => {
@@ -371,5 +455,17 @@ btnRight.addEventListener('click', () => {
 document.addEventListener('keydown', (event) => {
   if (event.key === "ArrowRight") {
     moveRight();
+  }
+})
+
+// ------------------- MOVE LEFT -------------------
+// Move Left when the left button is clicked
+btnLeft.addEventListener('click', () => {
+  moveLeft();
+})
+// Move Left when the left arrow key is pressed
+document.addEventListener('keydown', (event) => {
+  if (event.key === "ArrowLeft") {
+    moveLeft();
   }
 })
